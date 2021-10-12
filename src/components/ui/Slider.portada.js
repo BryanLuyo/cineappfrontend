99dbe26f4987c609d33f86774c7dd79e87360f09
Bryan Andy LC <bryanandy041965230@gmail.com>
2021-10-12 12:22:25 -0500

    css

--- src/components/ui/Slider.portada.js
+++ src/components/ui/Slider.portada.js
@@ -90,19 +90,9 @@ export const SliderPortada = () => {
               <span><a href=".">ver más <img src="/assets/view.png" alt="" /></a></span>
             </div>
             <div className="col-lg-12">
-            <div class="owl-carousel owl-theme">
-                  <div class="item"><h4>1</h4></div>
-                  <div class="item"><h4>2</h4></div>
-                  <div class="item"><h4>3</h4></div>
-                  <div class="item"><h4>4</h4></div>
-                  <div class="item"><h4>5</h4></div>
-                  <div class="item"><h4>6</h4></div>
-                  <div class="item"><h4>7</h4></div>
-                  <div class="item"><h4>8</h4></div>
-                  <div class="item"><h4>9</h4></div>
-                  <div class="item"><h4>10</h4></div>
-                  <div class="item"><h4>11</h4></div>
-                  <div class="item"><h4>12</h4></div>
+            <div className="owl-carousel owl-theme">
+                  <div className="item"><h4>1</h4></div>
+                  <div className="item"><h4>2</h4></div>                  
               </div>
             </div>
                        
@@ -127,6 +117,20 @@ export const SliderPortada = () => {
         </div>
       </div>
 
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+                <ul>
+                  <li><a>Condiciones de uso y seguridad</a></li>
+                  <li><a>Información financiera</a></li>
+                  <li><a>Hechos de importancia</a></li>
+                  <li><a>Libro de Reclamaciones</a></li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
 
     </>
   );
